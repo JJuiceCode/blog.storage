b401ee1ec278fded67bddfd36d650d7ff7ae0aad
jjuicecode <juk1126@gmail.com>
2022-01-25 18:40:56 +0900

    네비게이션 영어 → 한글로 수정 Tue Jan 25 18:40:56     2022

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd public
 # Add changes to git.
 git add .
 # Commit changes.
-msg="네비게이션 메뉴 수정 `date`"
+msg="네비게이션 영어 → 한글로 수정 `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -16,7 +16,7 @@ git push origin master
 cd ..
 # blog 저장소 Commit & Push
 git add .
-msg="네비게이션 메뉴 수정 `date`"
+msg="네비게이션 영어 → 한글로 수정 `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
