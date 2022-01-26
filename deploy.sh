1cf7fe43788de8bc42664d18f9bf6244a5893711
jjuicecode <juk1126@gmail.com>
2022-01-26 15:54:23 +0900

    subscription 기능 false Wed Jan 26 15:54:22     2022

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd public
 # Add changes to git.
 git add .
 # Commit changes.
-msg="post-header와 title겹쳐보이는 부분 수정 `date`"
+msg="subscription 기능 false `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -16,7 +16,7 @@ git push origin master
 cd ..
 # blog 저장소 Commit & Push
 git add .
-msg="post-header와 title겹쳐보이는 부분 수정 `date`"
+msg="subscription 기능 false `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
