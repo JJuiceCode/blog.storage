65c4c88d3101da008e6ef74e0691b287b69ecec2
jjuicecode <juk1126@gmail.com>
2022-01-26 15:16:08 +0900

    params data 수정 Wed Jan 26 15:16:07     2022

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd public
 # Add changes to git.
 git add .
 # Commit changes.
-msg="root _index.md title descrpition 수정 `date`"
+msg="params data 수정 `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
@@ -16,7 +16,7 @@ git push origin master
 cd ..
 # blog 저장소 Commit & Push
 git add .
-msg="root _index.md title descrpition 수정 `date`"
+msg="params data 수정 `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
